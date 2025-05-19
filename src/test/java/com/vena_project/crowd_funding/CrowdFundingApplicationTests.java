b415485ef65245af195a81589a97d6b97b938fd6
Anurag Gupta <agupta@venacorp.com>
2025-05-19 14:42:30 +0530

    barebone project

---
+++ src/test/java/com/vena_project/crowd_funding/CrowdFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vena_project.crowd_funding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdFundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
