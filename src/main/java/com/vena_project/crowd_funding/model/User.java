797ab938c12e8d16d379e42c4901f7143c941cc8
Anurag Gupta <agupta@venacorp.com>
2025-05-19 16:31:19 +0530

    user entity created

--- src/main/java/com/vena_project/crowd_funding/model/User.java
+++ src/main/java/com/vena_project/crowd_funding/model/User.java
@@ -1,4 +1,36 @@
 package com.vena_project.crowd_funding.model;
 
+import com.vena_project.crowd_funding.model.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "name cannot be empty string or null")
+    private String name;
+
+    @Email(message = "provide a valid email address")
+    @NotBlank
+    private String email;
+
+    @NotBlank(message = "provide a valid password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "createdBy")
+    private List<Project> projects = new ArrayList<>();
 }
