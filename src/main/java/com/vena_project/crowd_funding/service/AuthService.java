f56bab0adf1f5e820f161fd82883688040a8c63f
Anurag Gupta <agupta@venacorp.com>
2025-05-20 11:33:11 +0530

    removed auth controller, service

--- src/main/java/com/vena_project/crowd_funding/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.vena_project.crowd_funding.service;
-
-import com.vena_project.crowd_funding.dto.LoginRequest;
-import com.vena_project.crowd_funding.model.User;
-
-public interface AuthService {
-    void register(User user);
-    void login(LoginRequest loginRequest);
-}
--- src/main/java/com/vena_project/crowd_funding/service/UserService.java
+++ src/main/java/com/vena_project/crowd_funding/service/UserService.java
@@ -1,5 +1,6 @@
 package com.vena_project.crowd_funding.service;
 
+import com.vena_project.crowd_funding.dto.LoginRequest;
 import com.vena_project.crowd_funding.dto.UpdateUserInfo;
 import com.vena_project.crowd_funding.model.User;
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface UserService {
     List<User> usersList();
     void updateUserInformation(UpdateUserInfo updatedUserInfo);
+    void register(User user);
+    void login(LoginRequest loginRequest);
 }
