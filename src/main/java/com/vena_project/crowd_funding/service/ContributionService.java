733fa3cce79c6e847f4b48257d39a8842e9fbca6
atirole <atirole@venacorp.com>
2025-05-20 12:06:34 +0530

    Define addInvestment and addDonation methods in ContributionService interface

--- src/main/java/com/vena_project/crowd_funding/service/ContributionService.java
+++ src/main/java/com/vena_project/crowd_funding/service/ContributionService.java
@@ -1,4 +1,15 @@
 package com.vena_project.crowd_funding.service;
 
+import com.vena_project.crowd_funding.dto.ContributionRequestDTO;
+import com.vena_project.crowd_funding.dto.ContributionResponseDTO;
+
 public interface ContributionService {
+
+    ContributionResponseDTO addInvestment(ContributionRequestDTO contributionRequestDTO);
+
+    ContributionResponseDTO addDonation(ContributionRequestDTO contributionRequestDTO);
+
+    // For future use:
+    // List<ContributionResponseDTO> getContributionsByProjectOwner(Long ownerId);
 }
+
