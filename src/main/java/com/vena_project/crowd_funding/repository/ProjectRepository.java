251ca83b23517799abdaad688a93c808dc680267
atirole <atirole@venacorp.com>
2025-05-20 17:10:08 +0530

    Refactored ProjectRepository and adjusted related module code to meet current requirements and prevent merge conflicts

--- src/main/java/com/vena_project/crowd_funding/repository/ProjectRepository.java
+++ src/main/java/com/vena_project/crowd_funding/repository/ProjectRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository {
     // Optional: custom query methods if needed
 }
 
