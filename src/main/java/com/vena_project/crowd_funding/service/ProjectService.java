e1d2ebe1ad32fd7922a2a27e0b3e306f410cbd1e
Shraddha Singh <shraddhasinghbais4@gmail.com>
2025-05-21 23:21:09 +0530

    Merged dto

--- src/main/java/com/vena_project/crowd_funding/service/ProjectService.java
+++ src/main/java/com/vena_project/crowd_funding/service/ProjectService.java
@@ -1,15 +1,17 @@
 package com.vena_project.crowd_funding.service;
 
 import com.vena_project.crowd_funding.dto.*;
+import com.vena_project.crowd_funding.dto.RequestDTO.ProjectRequestDTO;
+import com.vena_project.crowd_funding.dto.ResponseDTO.ProjectResponseDTO;
 import com.vena_project.crowd_funding.model.Project;
 import java.util.List;
 
 public interface ProjectService {
     Project createProject(Long userId, ProjectRequestDTO project);
     List<ProjectResponseDTO> getProjectByUserId(Long createdBy);
-    List<ApprovedProjectDTO> getApprovedProjects();
-    ProjectDTO getProjectById(Long id);
-    List<ProjectTypeDTO> getProjectsByProfitability(boolean profitability);
+    List<ProjectDTO> getApprovedProjects();
+    ProjectResponseDTO getProjectById(Long id);
+    List<ProjectDTO> getProjectsByProfitability(boolean profitability);
     void deleteProject(Long projectId);
     Project findProjectById(Long projectId);
     Project saveProject(Project project);
