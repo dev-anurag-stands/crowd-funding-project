e8666dda659b56b5b48341a04c76fa06d76bf2d3
atirole <atirole@venacorp.com>
2025-05-22 22:09:26 +0530

    Add restriction to prevent users from contributing to their own projects and allowed access to admin to see all contributions on particular project by projectid

--- src/main/java/com/vena_project/crowd_funding/service/impl/ContributionServiceImpl.java
+++ src/main/java/com/vena_project/crowd_funding/service/impl/ContributionServiceImpl.java
@@ -50,6 +50,10 @@ public class ContributionServiceImpl implements ContributionService {
             logger.warn("Admin tried to contribute. Access denied.");
             throw new AccessDeniedForAdminException("Admins are not allowed to make contributions.");
         }
+        if (projectEntity.getCreatedBy().getId().equals(dto.getUserId())) {
+            logger.warn("User ID {} attempted to contribute to their own project ID {}. Access denied.", dto.getUserId(), projectId);
+            throw new IllegalArgumentException("You cannot contribute to a project you created.");
+        }
         if (projectEntity.getProjectStatus() != ProjectStatus.APPROVED) {
             logger.warn("Contribution rejected - Project ID {} is not approved.", projectId);
             throw new IllegalArgumentException("User can only contribute to an approved project.");
@@ -105,16 +109,11 @@ public class ContributionServiceImpl implements ContributionService {
         User requester = userService.getUserById(requesterId);
         Project projectEntity = projectService.findProjectById(projectId);
 
-        if (requester.getRole() != UserRole.ADMIN) {
-            logger.warn("Unauthorized access attempt by non-admin user ID {}", requesterId);
-            throw new AccessDeniedForAdminException("Admin users do not own any projects and cannot view contributions.");
-        }
-
-        if (!projectEntity.getCreatedBy().getId().equals(requesterId)) {
-            logger.warn("Admin ID {} tried to access contributions of a project not created by them.", requesterId);
+        if (requester.getRole() != UserRole.ADMIN &&
+                !projectEntity.getCreatedBy().getId().equals(requesterId)) {
+            logger.warn("Unauthorized access by User ID {} for Project ID {}", requesterId, projectId);
             throw new IllegalArgumentException("User not authorized to view contributions on this project.");
         }
-
         List<Contribution> contributions = contributionRepository.findByProjectId(projectId);
         ProjectResponseDTO projectResponseDTO = projectService.getProjectById(projectId);
 
